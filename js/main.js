214a738740ba423e09bb7f5df73d01eeb4add051
ulises trivi <ulisesty2@gmail.com>
2021-03-08 18:35:02 -0300

    hice el slider

--- js/main.js
+++ js/main.js
@@ -18,16 +18,47 @@ menuDesplegable()
 //SLIDER
 const next = document.querySelector('#btn-next')
 const prev = document.querySelector('#btn-prev')
-const slides = document.querySelectorAll('div[data-slide]')
+const slides = document.querySelectorAll('#slider > div')
 
-next.addEventListener('click', e => {
-    for (let i = 0; i < slides.length;) {
-        if(slides[i].classList.contains('d-none') && i < slides.length - 1) {
-            slides[i].classList.remove('d-none')
-        } else {
-            slides[0].classList.add('d-none')
-        }
-        i++
+next.addEventListener('click', function(e) {
+    if((slides[1].classList.contains('d-none') && slides[2].classList.contains('d-none')) && !slides[0].classList.contains('d-none')) {
+        slides[0].classList.add('d-none')
+        slides[1].classList.remove('d-none')
+    } else if((slides[0].classList.contains('d-none') && slides[2].classList.contains('d-none')) && !slides[1].classList.contains('d-none')) {
+        slides[1].classList.add('d-none')
+        slides[2].classList.remove('d-none')
+    } else if(!slides[2].classList.contains('d-none') && (slides[0].classList.contains('d-none') && slides[1].classList.contains('d-none'))) {
+        slides[0].classList.remove('d-none')
+        slides[2].classList.add('d-none')
     }
 })
 
+/* if(slides[i + 1].classList.contains('d-none')) {
+    slides[i + 1].classList.remove('d-none')
+    slides[i].classList.add('d-none')
+    break
+}
+
+if(slides[0].classList.contains('d-none') && slides[2].classList.contains('d-none') && !slides[1].classList.contains('d-none')) {
+    slides[2].classList.remove('d-none')
+    slides[1].classList.add('d-none')
+} */
+
+
+
+
+
+/* next.addEventListener('click', e => {
+    for (let i = 0; i < slides.length;) {        
+        if(!slides[i].classList.contains('d-none')) {
+            slides[i].classList.add('d-none')
+            slides[i + 1].classList.remove('d-none')
+        } else {
+            slides[i + 1].classList.add('d-none')//SE LE SACA D-NONE AL SIGUIENTE ELEMENTO BOLUDASO!
+        }
+        slides[i + 1]
+        i++
+    }
+    console.log(e)
+})
+ */
